4aeadbd8ed147e8d9e2015ccf73d5cce084b61cb
kinaciof <krystianoinacio@gmail.com>
2026-03-05 18:58:42 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Dono da Oficina</h1>
+      <p>O seu site está online!</p>
+    </main>
+  )
+}
